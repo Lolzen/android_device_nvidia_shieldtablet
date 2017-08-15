fe286c5632d564d1faaae0ea5c609a78081dc620
Lolzen <funer@gmx.at>
2017-08-15 17:02:25 +0200

    this is OmniROM

--- omni_shieldtablet.mk
+++ omni_shieldtablet.mk
@@ -1,8 +1,8 @@
 # Inherit device configuration for shieldtablet.
 $(call inherit-product, device/nvidia/shieldtablet/full_shieldtablet.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common omni stuff.
+$(call inherit-product, vendor/omni/config/common_tablet.mk)
 
-PRODUCT_NAME := lineage_shieldtablet
+PRODUCT_NAME := omni_shieldtablet
 PRODUCT_DEVICE := shieldtablet
